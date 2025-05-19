53372a2ea24488e55ce3876d81c476ae7113f7be
Jaddancel <jaddancel@gmail.com>
2025-05-19 10:55:56 +0800

    new file:   config.php 	new file:   index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+    include 'config.php';
+    // access table "products" from database and put items in array
+    $sql = "SELECT * FROM products";
+    $result = mysql_query($sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container-md mt-5">
+        <div class="d-flex justify-content-between align-items-center">
+            <h1>Computer Shop</h1>
+            <button class="btn btn-success">Log In</button>
+        </div>
+        <hr>
+        
+        <div class="row row-cols-2 row-cols-md-4 g-4">
+            <?php
+            while ($row = mysql_fetch_array($result)){
+                echo '<div class="col">
+                        <div class="card h-100" style="width: 100%;">
+                            <img class="card-img-top" src="'. $row['product_image_url'] .'" alt="Card image">
+                            <div class="card-body">
+                                <h4 class="card-title">"'. $row['product_name'] .'"</h4>
+                                <p class="card-text">"'. $row['product_price'] .'".</p>
+                                <a href="#" class="btn btn-primary">See</a>
+                            </div>
+                        </div>
+                    </div>';
+            }
+            ?>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
